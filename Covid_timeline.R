b95820f4f27450c3e0fe67bb86464378d69f8fe4
Aviraj Sinha <avirajs@smu.edu>
2020-06-21 15:50:51 -0500

    Get infections per day from time line data
    first convert from long to wide representation, apply diff on each column then melts back to long form,

--- Covid_timeline.R
+++ Covid_timeline.R
@@ -17,7 +17,21 @@ ggplot(cases_US, aes(x = date, y = confirmed_cases)) + geom_line() + geom_smooth
 cases_confirmed_by_county <- cases_TX %>% select(confirmed_cases,county_name, date) %>%
   drop_na() %>% group_by(county_name, date) %>% summarize(cases_per_day = c(confirmed_cases[1],diff(confirmed_cases)) )
 
+
+#cumulative
 ggplot(cases_confirmed_by_county, aes(x = date, y = cases_per_day)) +
  geom_line(aes(color = county_name, linetype = county_name), show.legend = FALSE  ) +
  geom_smooth(aes(color = county_name, linetype = county_name), show.legend = FALSE, se=FALSE)+
  ylim(0,100)
+
+#first convert from long to wide representation, apply diff on each column then melts back to long form, 
+#TODO must get dates labels back
+
+cases_confirmed_by_county <- cases_TX %>% select(county_name, date, confirmed_cases) %>%
+ group_by(county_name, date) %>% dcast(date ~ county_name,value.var="confirmed_cases")  %>% select(-date)%>%sapply( diff, lag=1) %>%
+  melt(id.vars = c("date", "county_name"), measure.vars = c("cases_per_day")) %>%   drop_na()
+
+
+ggplot(cases_confirmed_by_county, aes(x = Var1, y = value)) +
+ # geom_line(aes(color = Var2, linetype = Var2), show.legend = FALSE  ) +
+ geom_smooth(aes(color = Var2, linetype = Var2), show.legend = FALSE, se=FALSE) + ylim(0,20)
