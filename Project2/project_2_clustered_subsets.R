3943c4c4831aff3ea8d0d78cd0fda95b45c77058
Aviraj Sinha <avirajs@Avirajs-MacBook-Pro.local>
2020-07-13 13:16:49 -0500

    Added all internal measures from fpc, and a few optimal clusters methods, added texas secific charts and dbscan,
    added seriation as well and images of distances and basic pca 
    visualization as well;
    
    should be easier to choose subset before the clustering too.

--- Project2/project_2_clustered_subsets.R
+++ Project2/project_2_clustered_subsets.R
@@ -50,48 +50,96 @@ cases_select <- na.omit(cases_select)
 
 
 #scale
-cases_select_scaled <- cases_select %>% keep(is.numeric) %>% scale()
-cor(cases_select_scaled)
-install.packages(fpc)
-
+library(dbscan)
 library(cluster)
+# library(fpc)
+
+#select a subset and run the code below
+
 #all variable clustering
+cases_select_scaled <- cases_select %>% keep(is.numeric) %>% scale()
+
+#impact of health on death and deaths per cases
+cases_select_scaled <- cases_select %>% keep(is.numeric) %>% scale() %>% as.tibble %>%
+  select(deaths_per_1000,death_per_case,icu_bed_pop,primary_care_1000,median_age)
+
+#impact of demographics on death and deaths per cases
+selection_demographics <- cases_select %>% keep(is.numeric) %>% scale() %>%
+ as.tibble %>% select(cases_per_1000,deaths_per_1000,percent_black,unemployment_rate,population_density,median_income)
+
+
+
+
+
+
+
+
+ks <- 2:15
+
+
+
+#selecting cluster number
+
+#silouete
+ASW <- sapply(ks, FUN=function(k) {
+  fpc::cluster.stats(d, kmeans(cases_select_scaled, centers=k, nstart=5)$cluster)$avg.silwidth
+  })
+#choosing optimal number of clusters
+best_k <- ks[which.max(ASW)]
+ggplot(as_tibble(ks, ASW), aes(ks, ASW)) + geom_line() +
+  geom_vline(xintercept = 2, color = "red", linetype = 2)
+
+
+#dun index
+DI <- sapply(ks, FUN=function(k) {
+  fpc::cluster.stats(d, kmeans(cases_select_scaled, centers=k, nstart=5)$cluster)$dunn
+})
+best_k <- ks[which.max(DI)]
+ggplot(as_tibble(ks, DI), aes(ks, DI)) + geom_line() +
+  geom_vline(xintercept = best_k, color = "red", linetype = 2)
+
+
+#gap statistic
+k <- clusGap(cases_select_scaled, FUN = kmeans,  nstart = 10, K.max = 10)
+plot(k)
+
+
 
 
 
 #kmeans
-km <- kmeans(cases_select_scaled, centers = 3)
-
+km <- kmeans(cases_select_scaled, centers = 2)
+#internel cluster validation
+fpc::cluster.stats(d, km$cluster)
 #visualize cluster charactersitics
 ggplot(pivot_longer(as_tibble(km$centers,  rownames = "cluster"),
   cols = colnames(km$centers)),
   aes(y = name, x = value)) +
   geom_bar(stat = "identity") +
   facet_grid(rows = vars(cluster))
-
 #sillouete for kmeans
 d <- dist(cases_select_scaled)
 plot(silhouette(km$cluster, d))
-
-
-
-
-
-
+# all usa
 counties <- as_tibble(map_data("county"))
 cases_select <- cases_select %>% mutate(county = county_name %>%
   str_to_lower() %>% str_replace('\\s+county\\s*$', ''))
-
-
-
-# all usa
 us_counties <- counties %>%
   rename(c(county = subregion))
-
+#visualize map
 us_counties_clust <- us_counties %>% left_join(cases_select %>%
     add_column(cluster = factor(km$cluster)))
+ggplot(us_counties_clust, aes(long, lat)) +
+  geom_polygon(aes(group = group, fill = cluster)) +
+  coord_quickmap() +
+  labs(title = "Clusters", subtitle = "Only counties reporting 10+ cases")
 
-#visualize map
+#see texas zoomed
+counties_TX <- counties %>% dplyr::filter(region == "texas") %>%
+  rename(c(county = subregion))
+
+us_counties_clust <- counties_TX %>% left_join(cases_select %>%
+    add_column(cluster = factor(km$cluster)))
 ggplot(us_counties_clust, aes(long, lat)) +
   geom_polygon(aes(group = group, fill = cluster)) +
   coord_quickmap() +
@@ -99,132 +147,67 @@ ggplot(us_counties_clust, aes(long, lat)) +
 
 
 
-
-
+#hierarcal clustering
 d <- dist(cases_select_scaled)
 hc <- hclust(d)
-cl <- cutree(hc, k = 8)
-
-
+cl <- cutree(hc, k = 12)
+#internal cluster validation
+fpc::cluster.stats(d, hc$cluster)
+#visualize map
 us_counties_clust <- us_counties %>% left_join(cases_select %>%
     add_column(cluster = factor(cl)))
-
 ggplot(us_counties_clust, aes(long, lat)) +
   geom_polygon(aes(group = group, fill = cluster)) +
   coord_quickmap() +
   labs(title = "Clusters", subtitle = "Only counties reporting 10+ cases")
-
-
-
-
-
-# all texas counties
-# counties_TX <- counties %>% dplyr::filter(region == "texas") %>%
-#   rename(c(county = subregion))
-
-
-
-
-
-
-
-
-#impact of health on death and deaths per cases
-selection_health <- cases_select_scaled %>% as.tibble %>% select(deaths_per_1000,death_per_case,icu_bed_pop,primary_care_1000,median_age)
-
-
-
-km <- kmeans(selection_health, centers = 3)
-
-#visualize cluster charactersitics
-ggplot(pivot_longer(as_tibble(km$centers,  rownames = "cluster"),
-  cols = colnames(km$centers)),
-  aes(y = name, x = value)) +
-  geom_bar(stat = "identity") +
-  facet_grid(rows = vars(cluster))
-  us_counties <- counties %>%
-    rename(c(county = subregion))
-
-#sillouete for kmeans
-d <- dist(selection_health)
-plot(silhouette(km$cluster, d))
-
-#visualize map
-ggplot(us_counties_clust, aes(long, lat)) +
-  geom_polygon(aes(group = group, fill = cluster)) +
-  coord_quickmap() +
-  labs(title = "Clusters", subtitle = "Only counties reporting 10+ cases")
-
-
-
-
-#hierarch clustering health
-d <- dist(selection_health)
-hc <- hclust(d)
-
-# plot(hc)
-
-cl <- cutree(hc, k = 8)
-
-ggplot(us_counties_clust, aes(long, lat)) +
-  geom_polygon(aes(group = group, fill = cluster)) +
-  coord_quickmap() +
-  labs(title = "Clusters", subtitle = "Only counties reporting 10+ cases")
-
-
-
-
-
-
-
-
-
-
-
-
-#impact of demographics on death and deaths per cases
-selection_demographics <- cases_select_scaled %>% as.tibble %>% select(cases_per_1000,deaths_per_1000,percent_black,unemployment_rate,population_density,median_income)
-
-
-
-km <- kmeans(selection_demographics, centers = 3)
-
-#visualize cluster charactersitics
-ggplot(pivot_longer(as_tibble(km$centers,  rownames = "cluster"),
-  cols = colnames(km$centers)),
-  aes(y = name, x = value)) +
-  geom_bar(stat = "identity") +
-  facet_grid(rows = vars(cluster))
-  us_counties <- counties %>%
-    rename(c(county = subregion))
-
-#sillouete for kmeans
-d <- dist(selection_demographics)
-plot(silhouette(km$cluster, d))
-
-#visualize map
-ggplot(us_counties_clust, aes(long, lat)) +
-  geom_polygon(aes(group = group, fill = cluster)) +
-  coord_quickmap() +
-  labs(title = "Clusters", subtitle = "Only counties reporting 10+ cases")
-
-
-
-
-
-
-
-#hierarch clustering demo
-d <- dist(selection_demographics)
-hc <- hclust(d)
-# plot(hc)
-
-cl <- cutree(hc, k = 8)
-
-us_counties_clust <- us_counties %>% left_join(cases_select %>%
+#texas
+us_counties_clust <- counties_TX %>% left_join(cases_select %>%
     add_column(cluster = factor(cl)))
-
 ggplot(us_counties_clust, aes(long, lat)) +
   geom_polygon(aes(group = group, fill = cluster)) +
   coord_quickmap() +
   labs(title = "Clusters", subtitle = "Only counties reporting 10+ cases")
+
+
+
+
+#dbscan clustering
+kNNdistplot(cases_select_scaled, k = 10)
+abline(h=3, col="red")
+
+db <- dbscan(cases_select_scaled, eps=3)
+db
+
+#internel cluster validation
+fpc::cluster.stats(d, db$cluster)
+
+#visualize map
+us_counties_clust <- us_counties %>% left_join(cases_select %>%
+    add_column(cluster = factor(db$cluster)))
+ggplot(us_counties_clust, aes(long, lat)) +
+  geom_polygon(aes(group = group, fill = cluster)) +
+  coord_quickmap() +
+  labs(title = "Clusters", subtitle = "Only counties reporting 10+ cases")
+
+us_counties_clust <- counties_TX %>% left_join(cases_select %>%
+    add_column(cluster = factor(db$cluster)))
+ggplot(us_counties_clust, aes(long, lat)) +
+  geom_polygon(aes(group = group, fill = cluster)) +
+  coord_quickmap() +
+  labs(title = "Clusters", subtitle = "Only counties reporting 10+ cases")
+
+
+library(seriation)
+
+pimage(d)
+dissplot(d, labels = km$cluster, options=list(main="k-means with k=2"),col = bluered(100,bias = 0.1))
+dissplot(d, labels = cl, options=list(main="Hierarchal with 12"),col = greenred(100,bias = 0.1))
+
+pimage(d, order=order(db$cluster), col = bluered(100,bias = 0.1))
+
+library(factoextra)
+
+pca_res <- prcomp(cases_select_scaled, scale. = TRUE)
+
+library(ggfortify)
+autoplot(pca_res)
